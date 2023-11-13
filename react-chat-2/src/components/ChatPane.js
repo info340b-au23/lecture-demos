421bf384c9dddd9e935bb1737f0579550fd0adb2
Tim Carlson <timca@arenasports.net>
2023-11-13 10:23:26 -0800

    post chat2 lecture

--- react-chat-2/src/components/ChatPane.js
+++ react-chat-2/src/components/ChatPane.js
@@ -1,46 +1,46 @@
-import React from 'react';
+// import React from 'react';
 
-import CHAT_HISTORY from '../data/chat_log.json';
+// import CHAT_HISTORY from '../data/chat_log.json';
 
-export function ChatPane(props) {
-  const currentChannel = props.currentChannel;
+// export function ChatPane(props) {
+//   const currentChannel = props.currentChannel;
 
-  const handleClick = (event) => {
-    console.log("you clicked me!");
-  }
+//   const handleClick = (event) => {
+//     console.log("you clicked me!");
+//   }
 
-  //only show current channel messages
-  const channelMessage = CHAT_HISTORY.filter((msgObj) => {
-    return msgObj.channel === currentChannel;
-  })
+//   //only show current channel messages
+//   const channelMessage = CHAT_HISTORY.filter((msgObj) => {
+//     return msgObj.channel === currentChannel;
+//   })
 
-  const messageItemArray = channelMessage.map((messageObj) => {
-    const element = (
-      <MessageItem
-        messageData={messageObj}
-        key={messageObj.timestamp}
-      />
-    )
-    return element;
-  })
+//   const messageItemArray = channelMessage.map((messageObj) => {
+//     const element = (
+//       <MessageItem
+//         messageData={messageObj}
+//         key={messageObj.timestamp}
+//       />
+//     )
+//     return element;
+//   })
 
 
-  // original 
-  return (
-    <div className="scrollable-pane">
-      <div className="pt-2 my-2">
-        {/* testing button */}
-        <button className="btn btn-outline-primary mb-3" onClick={handleClick}>
-          Click me!
-        </button>
-        <hr/>
+//   // // original 
+//   // return (
+//   //   <div className="scrollable-pane">
+//   //     <div className="pt-2 my-2">
+//   //       {/* testing button */}
+//   //       <button className="btn btn-outline-primary mb-3" onClick={handleClick}>
+//   //         Click me!
+//   //       </button>
+//   //       <hr/>
 
-        {/* messages */}
-        {messageItemArray}
-      </div>
-    </div>
-    )
-  }
+//   //       {/* messages */}
+//   //       {messageItemArray}
+//   //     </div>
+//   //   </div>
+//   //   )
+//   // }
 
 //   // Slide 13 - show the button if 'general' is the current channel
 //   return (
@@ -63,30 +63,30 @@ export function ChatPane(props) {
 // }
 
 
-function MessageItem(props) {
-  const { userName, userImg, text } = props.messageData;
-  // const userName = props.messageData.userName;
-  // const userImg = props.messageData.userImg;
+// function MessageItem(props) {
+//   const { userName, userImg, text } = props.messageData;
+//   // const userName = props.messageData.userName;
+//   // const userImg = props.messageData.userImg;
 
-  const handleClick = (event) => {
-    console.log("you liked " + userName + "'s post!");
-  }
+//   const handleClick = (event) => {
+//     console.log("you liked " + userName + "'s post!");
+//   }
 
-  return (
-    <div className="message d-flex mb-3">
-      <div className="me-2">
-        <img src={userImg} alt={userName + "'s avatar"} />
-      </div>
-      <div className="flex-grow-1">
-        <p className="user-name">{userName}</p>
-        <p>{text}</p>
-        <button className="btn like-button" onClick={handleClick}>
-          <span className="material-icons" style={{ color: "grey" }}>favorite_border</span>
-        </button>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="message d-flex mb-3">
+//       <div className="me-2">
+//         <img src={userImg} alt={userName + "'s avatar"} />
+//       </div>
+//       <div className="flex-grow-1">
+//         <p className="user-name">{userName}</p>
+//         <p>{text}</p>
+//         <button className="btn like-button" onClick={handleClick}>
+//           <span className="material-icons" style={{ color: "grey" }}>favorite_border</span>
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
 
 
 // // Slide 14 and 16 - conditional rendering and initial event handling
@@ -167,92 +167,92 @@ function MessageItem(props) {
 //   )
 // }
 
-// // Slide 21 and 22 - Using State Hooks
-// import React, { useState } from 'react';
-// import CHAT_HISTORY from '../data/chat_log.json';
+// Slide 21 and 22 - Using State Hooks
+import React, { useState } from 'react';
+import CHAT_HISTORY from '../data/chat_log.json';
 
-// export function ChatPane(props) {
+export function ChatPane(props) {
  
-//   const stateManagementArray = useState(0); //start out at 0 very first time
-//   console.log("stateManagementArray: ", stateManagementArray);
-//   const count = stateManagementArray[0];
-//   const setCount = stateManagementArray[1];
+  // const stateManagementArray = useState(0); //start out at 0 very first time
+  // console.log("stateManagementArray: ", stateManagementArray);
+  // const count = stateManagementArray[0];
+  // const setCount = stateManagementArray[1];
 
-//   // //instead destructure
-//   // const [count, setCount] = useState(0);
-//   const currentChannel = props.currentChannel;
+  //instead destructure
+  const [count, setCount] = useState(0);
+  const currentChannel = props.currentChannel;
 
 
-//   const handleClick = (event) => {
-//     setCount(count + 1);
-//     console.log("you clicked me!", count);
-//   }
+  const handleClick = (event) => {
+    setCount(count + 1);
+    console.log("you clicked me!", count);
+  }
 
-//   //only show current channel messages
-//   const channelMessages = CHAT_HISTORY.filter((msgObj) => {
-//     return msgObj.channel === currentChannel;
-//   })
+  //only show current channel messages
+  const channelMessages = CHAT_HISTORY.filter((msgObj) => {
+    return msgObj.channel === currentChannel;
+  })
 
-//   //IF THERE ARE NO MESSAGES IN THIS CHANNEL, SHOW THIS
-//   if (channelMessages.length === 0) {
-//     return <p>No Messages Yet</p>
-//   }
-//   const messageItemArray = channelMessages.map((messageObj) => {
-//     const element = (
-//       <MessageItem
-//         messageData={messageObj}
-//         key={messageObj.timestamp}
-//       />
-//     )
-//     return element;
-//   })
+  //IF THERE ARE NO MESSAGES IN THIS CHANNEL, SHOW THIS
+  if (channelMessages.length === 0) {
+    return <p>No Messages Yet</p>
+  }
+  const messageItemArray = channelMessages.map((messageObj) => {
+    const element = (
+      <MessageItem
+        messageData={messageObj}
+        key={messageObj.timestamp}
+      />
+    )
+    return element;
+  })
 
-//   return (
-//     <div className="scrollable-pane">
-//       <div className="pt-2 my-2">
-//         {/* testing button */}
-//         {currentChannel === 'general' &&
-//           <div>
-//             <button className="btn btn-outline-primary mb-3" onClick={handleClick}>
-//               Click me: {count}
-//             </button>
-//             <hr />
-//           </div>}
-//         {/* messages */}
-//         {messageItemArray}
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div className="scrollable-pane">
+      <div className="pt-2 my-2">
+        {/* testing button */}
+        {currentChannel === 'general' &&
+          <div>
+            <button className="btn btn-outline-primary mb-3" onClick={handleClick}>
+              Click me: {count}
+            </button>
+            <hr />
+          </div>}
+        {/* messages */}
+        {messageItemArray}
+      </div>
+    </div>
+  )
+}
 
-// function MessageItem(props) {
-//   const { userName, userImg, text } = props.messageData;
-//   const [isLiked, setIsLiked] = useState(false);
+function MessageItem(props) {
+  const { userName, userImg, text } = props.messageData;
+  const [isLiked, setIsLiked] = useState(false);
 
-//   const handleClick = (event) => {
-//     console.log("you liked " + userName + "'s post!");
-//     setIsLiked(!isLiked);
-//   }
+  const handleClick = (event) => {
+    console.log("you liked " + userName + "'s post!");
+    setIsLiked(!isLiked);
+  }
 
-//   let heartColor = 'grey';
-//   if (isLiked) {
-//     heartColor = 'red';
-//   }
-//   return (
-//     <div className="message d-flex mb-3">
-//       <div className="me-2">
-//         <img src={userImg} alt={userName + "'s avatar"} />
-//       </div>
-//       <div className="flex-grow-1">
-//         <p className="user-name">{userName}</p>
-//         <p>{text}</p>
-//         <button className="btn like-button" onClick={handleClick}>
-//           <span className="material-icons" style={{ color: heartColor }}>favorite_border</span>
-//         </button>
-//       </div>
-//     </div>
-//   )
-// }
+  let heartColor = 'grey';
+  if (isLiked) {
+    heartColor = 'red';
+  }
+  return (
+    <div className="message d-flex mb-3">
+      <div className="me-2">
+        <img src={userImg} alt={userName + "'s avatar"} />
+      </div>
+      <div className="flex-grow-1">
+        <p className="user-name">{userName}</p>
+        <p>{text}</p>
+        <button className="btn like-button" onClick={handleClick}>
+          <span className="material-icons" style={{ color: heartColor }}>favorite_border</span>
+        </button>
+      </div>
+    </div>
+  )
+}
 
 // //Slide 35 (36 and 37) to elevate state to App
 // import React, {useState} from 'react';
--- react-chat-2/src/components/ComposeForm.js
+++ react-chat-2/src/components/ComposeForm.js
@@ -1,55 +1,56 @@
-import React from 'react';
-
-export function ComposeForm(props) {
-
-  return (
-    <form className="my-2">
-      <div className="input-group">
-        <textarea className="form-control" rows="2" placeholder="Type a new message"></textarea>
-        <button className="btn btn-secondary" type="button">
-          <span className="material-icons">send</span>
-        </button>
-      </div>
-    </form>
-  );
-}
-
-// //Slide 31 React Form Inputs (example)
-// import React, { useState } from 'react';
+// import React from 'react';
 
 // export function ComposeForm(props) {
-
-//    // const typedValue = ""
-//   const [typedValue, setTypedValue] = useState("");
-
-//   const handleChange = (event) => {
-//     const value = event.target.value;
-//     console.log("user typed:", value);
-//     setTypedValue(value); //update state and rerender
-//   }
-
-//   const handleSubmit = (event) => {
-//     console.log("submit the form");
-//     console.log("post", typedValue);
-//     setTypedValue("")
-//   }
 
 //   return (
 //     <form className="my-2">
 //       <div className="input-group">
-//         <textarea
-//           className="form-control" rows="2"
-//           placeholder="Type a new message"
-//           value={typedValue}
-//           onChange={handleChange}>
-//         </textarea>
-//         <button className="btn btn-secondary" type="button" onClick={handleSubmit} >
+//         <textarea className="form-control" rows="2" placeholder="Type a new message"></textarea>
+//         <button className="btn btn-secondary" type="button">
 //           <span className="material-icons">send</span>
 //         </button>
 //       </div>
 //     </form>
 //   );
 // }
+
+//Slide 31 React Form Inputs (example)
+import React, { useState } from 'react';
+
+export function ComposeForm(props) {
+
+   // const typedValue = ""
+  const [typedValue, setTypedValue] = useState("");
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    // console.log("event: ", event)
+    console.log("user typed:", value);
+    setTypedValue(value); //update state and rerender
+  }
+
+  const handleSubmit = (event) => {
+    console.log("submit the form");
+    console.log("post", typedValue);
+    setTypedValue("")
+  }
+
+  return (
+    <form className="my-2">
+      <div className="input-group">
+        <textarea
+          className="form-control" rows="2"
+          placeholder="Type a new message"
+          value={typedValue}
+          onChange={handleChange}>
+        </textarea>
+        <button className="btn btn-secondary" type="button" onClick={handleSubmit} >
+          <span className="material-icons">send</span>
+        </button>
+      </div>
+    </form>
+  );
+}
 
 // //slide 34 - React Form Inputs Example
 // import React, { useState } from 'react';
