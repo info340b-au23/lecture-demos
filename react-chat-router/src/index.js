2966d5eb95f1b00f550f436e1ebfcc082b0356c5
Tim Carlson <timca@arenasports.net>
2023-11-20 06:57:15 -0800

    pre-lecture react-router

--- react-chat-router/src/index.js
+++ react-chat-router/src/index.js
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 // // Slide 16
+
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 
